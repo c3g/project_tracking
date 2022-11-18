5c52a0dde0119a3fcf09c760e8953a86934c05aa
P-O Quirion <pioliqui@gmail.com>
2022-11-18 00:19:02 -0500

    lets have session outside of app

--- c3g_project_tracking/database.py
+++ c3g_project_tracking/database.py
@@ -1,6 +1,7 @@
 """Module providing database tables and operations support."""
-import logging
 import click
+import logging
+import os
 
 import flask
 from sqlalchemy import (
@@ -11,25 +12,36 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
 
 
-def get_engine(db_uri=None):
+class Engine:
+    ENGINE = None
+    SESSION = None
 
-    if db_uri:
-        pass
-    elif flask.current_app.config["SQLALCHEMY_DATABASE_URI"]:
-        db_uri = flask.current_app.config["SQLALCHEMY_DATABASE_URI"]
-    else:
-        db_uri = '"sqlite+pysqlite:///:memory:"'
+def get_engine(db_uri):
 
     logging.info('Connecting to {}'.format(db_uri))
 
-    if 'engine' not in flask.g:
-        flask.g.engine = create_engine(db_uri, echo=True)
-    return flask.g.engine
+    if Engine.ENGINE is None:
+        Engine.ENGINE = create_engine(db_uri, echo=False)
+
+    return Engine.ENGINE
 
 
-def get_session():
+def get_session(no_app=False):
+    """
+    The no app option is a convenience to get a DB session outside of a flask app
+    """
+
+    if no_app:
+        if Engine.SESSION is None:
+            db_uri = os.getenv("SQLALCHEMY_DATABASE_URI", default="sqlite+pysqlite:///:memory:")
+            Engine.SESSION = sessionmaker(bind=get_engine(db_uri),
+                                          autoflush=False,
+                                          autocommit=False)
+        return Engine.SESSION
+
     if 'session' not in flask.g:
-        flask.g.session = scoped_session(sessionmaker(bind=get_engine(),
+        db_uri = flask.current_app.config["SQLALCHEMY_DATABASE_URI"]
+        flask.g.session = scoped_session(sessionmaker(bind=get_engine(db_uri=db_uri),
                                                       autoflush=False,
                                                       autocommit=False))
         Base = declarative_base()
@@ -37,10 +49,12 @@ def get_session():
     return flask.g.session
 
 
-def init_db():
+def init_db(db_uri=None):
     from . import model
-    engine = get_engine()
-    model.mapper_registry.metadata.create_all(engine)
+    if db_uri is None:
+        db_uri = flask.current_app.config["SQLALCHEMY_DATABASE_URI"]
+    engine = get_engine(db_uri)
+    model.reg.metadata.create_all(engine)
 
 
 def close_db(e=None):
