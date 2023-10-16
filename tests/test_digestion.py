f944a5353199e5f0eacceb0ebc5bf014f96b8655
Paul Stretenowich <paul.stretenowich@mcgill.ca>
2023-10-16 13:16:55 -0400

    Debug pytest

--- tests/test_digestion.py
+++ tests/test_digestion.py
@@ -13,11 +13,13 @@ from project_tracking import create_app
 logger = logging.getLogger(__name__)
 
 def test_digest_api(client, run_processing_json, readset_file_json, app):
-    response = client.get('admin/create_project/MOH-Q')
-    response = client.post('project/MOH-Q/ingest_run_processing', data=json.dumps(run_processing_json))
-    response = client.post('project/MOH-Q/digest_readset_file', data=json.dumps(readset_file_json))
+    project_name = run_processing_json[vb.PROJECT_NAME]
+    project_id = "1"
+    response = client.get(f'admin/create_project/{project_name}')
+    response = client.post(f'project/{project_id}/ingest_run_processing', data=json.dumps(run_processing_json))
+    response = client.post(f'project/{project_id}/digest_readset_file', data=json.dumps(readset_file_json))
     assert response.status_code == 200
-    response = client.post('project/MOH-Q/digest_pair_file', data=json.dumps(readset_file_json))
+    response = client.post(f'project/{project_id}/digest_pair_file', data=json.dumps(readset_file_json))
     assert response.status_code == 200
 
     with app.app_context():
